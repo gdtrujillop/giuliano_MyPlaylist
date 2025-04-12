59c5ca2b63ec84071c71aab60a489d6cfdb9ac5b
Giuliano Trujillo <didier1402@gmail.com>
2025-04-11 22:38:40 -0500

    Agregar archivos de proyecto.

---
+++ MyPlaylist/MasterClas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlaylist
+{
+    internal class MasterClas
+    {
+        private int Edad;
+        private string NombreCompleto;
+        private string Dni;
+
+
+        public MasterClas(int edad, string nombre, string dni)
+        {
+            Edad = edad;
+            NombreCompleto = nombre;
+            Dni = dni;
+        }
+
+
+        public int getEdad()
+        {
+            return Edad;
+        }
+
+        public void setEdad(int edad)
+        {
+            this.Edad = edad;
+        }
+
+
+        public String getNombreCompleto()
+        {
+            return NombreCompleto;
+        }
+
+        public void setNombreCompleto(string nombre)
+        {
+            this.NombreCompleto = nombre;
+        }
+
+        public String getDni()
+        {
+            return Dni;
+        }
+
+        public void setDni(string dni)
+        {
+            this.Dni = dni;
+        }
+
+    }
+
+}
---
+++ MyPlaylist/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyPlaylist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterClas mas = new MasterClas(38, "Giuliano", "12345678");
+            Console.WriteLine("Dni actual antes de setear nuevo: "+ mas.getDni());
+            Console.WriteLine(mas.getEdad());
+            mas.setDni("44069663");
+            Console.WriteLine("Nuevo Dni : "+mas.getDni());
+            Console.WriteLine(mas.getNombreCompleto());
+            
+        }
+    }
+}
